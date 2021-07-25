516472011946e398e0c858e6218014eb46b3fc22
Andrian-Aff <affear.123@gmail.com>
2021-07-25 15:29:00 +0300

    123

--- src/app.js
+++ src/app.js
@@ -63,3 +63,30 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryContainer = document.querySelector('.js-gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items.map(({preview, original, description}) => {
+    return `
+     <li class="gallery__item">
+    <a
+      class="gallery__link"
+        href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li> 
+  `
+  }).join('');
+}
+
+
